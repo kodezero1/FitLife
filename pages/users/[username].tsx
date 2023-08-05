411b86aeb938736124df255f8c73d03c86f11355
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-08-05 13:47:51 +0100

    Added weight tile

--- pages/users/[username].tsx
+++ pages/users/[username].tsx
@@ -14,6 +14,7 @@ import WorkoutTileList from '../../components/userProfile/WorkoutTileList'
 import LoadingBricks from '../../components/Shared/LoadingBricks'
 import { getSortedWorkoutLogDateArray } from '../../utils'
 import { User, Workout } from '../../types'
+import WeightTile from "../../components/settingsPage/WeightTile"
 
 const Username: React.FC = () => {
   const router = useRouter()
@@ -85,6 +86,7 @@ const Username: React.FC = () => {
           <LoadingBricks />
         </div>
       )}
+      <WeightTile/>
     </Container>
   )
 }
